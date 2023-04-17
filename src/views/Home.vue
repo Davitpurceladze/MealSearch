19eb3b894f3da687d9475ed6762442da10f03f2e
Davitpurceladze <purceladzedavit@gmail.com>
2023-04-17 23:08:26 +0400

    install axios and send request to server to get data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,10 +14,17 @@
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from '../store';
+import axiosClient from '../axiosClient.js'
 
 
-const meals = computed(() => store.state.meals)
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("")
+const ingredients = ref([])
+
+onMounted(async() => {
+  const response = await axiosClient.get('/list.php?i=list')
+  console.log(response.data)
+  ingredients.value = response.data
+})
 </script>
