b881ad846adb60d14b554a872a89c6dd2833321e
Davitpurceladze <purceladzedavit@gmail.com>
2023-04-29 14:16:27 +0400

    add title and some styles on mealsByLetter page

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -1,12 +1,17 @@
 <template>
-    <div class="flex justify-center gap-2 mt-2">
-      <router-link :to="{name: 'byLetter', params: {letter}}"  
-        v-for="letter of letters" 
-        :key='letter'>
-          {{ letter }}
-      </router-link>
-    </div>
-     <Meals :meals="meals"></Meals>
+  <div class="p-8 bp-0">
+    <h1 class="text-4xl font-bold mb-4 text-orange-500">Meals by Letter</h1>
+  </div>
+  <div class="flex justify-center gap-3 mt-px-8">
+    <router-link :to="{name: 'byLetter', params: {letter}}"  
+      v-for="letter of letters" 
+      :key='letter'
+      class="hover:text-orange-500 transition-colors"
+      >
+        {{ letter }}
+    </router-link>
+  </div>
+  <Meals :meals="meals"></Meals>
 </template>
 
 <script setup>
