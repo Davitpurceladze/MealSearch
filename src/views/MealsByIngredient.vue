58a5078d3542b21ff60356d4bd6f4c4aebbc7071
Davitpurceladze <purceladzedavit@gmail.com>
2023-04-29 14:10:13 +0400

    add title of searched meals in mealsByIngredient

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,14 +1,18 @@
 <template>
+  <div class="p-8 pb-0">
+    <h1 class="text-4xl font-bold mb-4 text-orange-500">Meals for {{ ingredient.strIngredient }}</h1>
+  </div>
   <Meals :meals="meals"></Meals>
 </template>
 
 <script setup>
 import Meals from '../components/Meals.vue'
-import { onMounted, ref, computed } from 'vue';
+import { onMounted,  computed } from 'vue';
 import store from '../store';
 import { useRoute } from 'vue-router';
 
 const route = useRoute()
+const ingredient = computed(() => store.state.ingredient)
 const meals = computed(() => store.state.mealsByIngredient)
 
 onMounted(() => {
