296982fd234d975351044a02418ae0518e16e4b2
Davitpurceladze <purceladzedavit@gmail.com>
2023-04-29 14:39:51 +0400

    change mealsByLetter page style

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -2,11 +2,11 @@
   <div class="p-8 bp-0">
     <h1 class="text-4xl font-bold mb-4 text-orange-500">Meals by Letter</h1>
   </div>
-  <div class="flex justify-center gap-3 mt-px-8">
+  <div class="flex flex-wrap justify-center gap-3 mt-px-8 mb-6">
     <router-link :to="{name: 'byLetter', params: {letter}}"  
       v-for="letter of letters" 
       :key='letter'
-      class="hover:text-orange-500 transition-colors"
+      class="hover:text-orange-500 transition-all w-2 h-2 flex items-center justify-center hover:scale-150"
       >
         {{ letter }}
     </router-link>
